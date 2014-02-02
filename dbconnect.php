f197b6c3c14bea552bf79edb19ab8b36621dafa6
vdganapathy <vdganapathy@gmail.com>
2014-02-02 17:28:37 +0530

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,8 +1,8 @@
 <?php
 $mysql_hostname = "localhost";
-$mysql_user = "shoppkd8_gana";
-$mysql_password = "v.d.Sudb888";
-$mysql_database = "shoppkd8_new";
+$mysql_user = "";
+$mysql_password = "";
+$mysql_database = "";
 $bd = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Opps some thing went wrong");
 mysql_select_db($mysql_database, $bd) or die("Opps some thing went wrong");
 ?>
